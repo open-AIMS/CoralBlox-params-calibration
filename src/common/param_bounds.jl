db3a45fd729c94a061bc75bbbde85169cd358e8d
Daniel Tan <d.tan@aims.gov.au>
2024-11-20 09:36:46 +1100

    resolve loading issues

--- src/common/param_bounds.jl
+++ src/common/param_bounds.jl
@@ -9,8 +9,49 @@ Define standard gl0obal variables for which this ordering and naming can be acce
 """
 function target_param_names()
     return [
-        "linear_extension", "mb_rate", "mean_colony_diameter_m", "fecundity", "dist_mean"
+        "linear_extension", "mb_rate", "fecundity", "dist_mean"
     ]
+end
+
+"""
+    sc_fg_param_idxs(param_name::String, coral_df::DataFrame):mean_colony_diameter_m:Vector{Int64}
+
+Return the indices of the parameter such that it returns them in the ordered used in ADRIA.
+
+Makes it easier to insert bounds into vector of sample bounds from a ordered source file.
+
+# Example
+
+```julia
+julia> ordered_idxs = sc_fg_param_idxs(param_name, coral_df)
+
+julia> coral_df[ordered_idxs, :].fieldname
+35-element Vector{Symbol}:
+ :tabular_Acropora_1_1_linear_extension
+ :tabular_Acropora_1_2_linear_extension
+ :tabular_Acropora_1_3_linear_extension
+...
+ :corymbose_Acropora_2_1_linear_extension
+...
+ :corymbose_non_Acropora_3_1_linear_extension
+...
+ :small_massives_4_1_linear_extension
+...
+ :large_massives_5_1_linear_extension
+...
+```
+
+"""
+function sc_fg_param_idxs(param_name::String, coral_df::DataFrame)::Vector{Int64}
+    idxs = zeros(Int64, 35)
+
+    n_taxa, n_sizes = size(ADRIA.bin_widths())
+    counter::Int64 = 1
+    for grp in 1:n_taxa, sc in 1:n_sizes
+        idxs[counter] = extract_param_group_idx(coral_df, "$(grp)_$(sc)_$(param_name)")[1]
+        counter += 1
+    end
+    return idxs
 end
 
 """
@@ -27,51 +68,87 @@ function adjust_bounds!(
     return nothing
 end
 
+"""
+    set_bounds!(sample_bounds, coral_param_idxs, lower_bounds::Vector{Float64}, upper_bounds::Vector{Float64})::Nothing
+"""
+function set_bounds!(
+    sample_bounds,
+    coral_param_idxs,
+    lower_bounds::Vector{Float64},
+    upper_bounds::Vector{Float64}
+)::Nothing
+    if any(lower_bounds .> upper_bounds)
+        msg = "At least one lower bound given is less then the corresponding upper bound"
+        throw(ArgumentError(msg))
+    end
+    sample_bounds[coral_param_idxs] .= collect(zip(lower_bounds, upper_bounds))
+    return nothing
+end
+
+function flatten_group_size(param_matrix::Matrix{Float64})::Vector{Float64}
+    n_taxa, n_sizes = size(param_matrix)
+    return reshape(permutedims(param_matrix, (2, 1)), (n_taxa * n_sizes,))
+end
+
+ecorrap_params = open_dataset(ECORRAP_PARAM_PATH)
+
+# Construct linear extension parameter bounds
+linear_extension_mean = ecorrap_params.filled_lin_ext_mean
+linear_extension_mean[:, 7] .= 0.0
+
+lin_ext_lb = linear_extension_mean.data[:, :] .* 0.9
+lin_ext_ub = linear_extension_mean.data[:, :] .* 1.1
+
+lin_ext_lb = flatten_group_size(lin_ext_lb)
+lin_ext_ub = flatten_group_size(lin_ext_ub)
+
+# Construct background mortality parameter bounds
+mb_rate_mean = ecorrap_params.filled_mb_rate_mean
+
+mb_rate_lb = mb_rate_mean.data[:, :] .* 0.9
+mb_rate_ub = mb_rate_mean.data[:, :] .* 1.1
+
+mb_rate_lb = flatten_group_size(mb_rate_lb)
+mb_rate_ub = flatten_group_size(mb_rate_ub)
+
 # Define parameter space to scan over
 coral_params = ADRIA.component_params(ADRIA.model_spec(dom), ADRIA.Coral)
 
 # Extract just the target coral parameters
-lin_ext_idx, mbrate_idx, coldiam_idx, fecundity_idx, dhw_tol_mean_idx =
+lin_ext_idx, mbrate_idx, fecundity_idx, dhw_tol_mean_idx =
     extract_param_group_idx.([coral_params], target_param_names())
 
 coral_param_idx = vcat(
-    lin_ext_idx, mbrate_idx, coldiam_idx, fecundity_idx, dhw_tol_mean_idx
+    lin_ext_idx, mbrate_idx, fecundity_idx, dhw_tol_mean_idx
 )
 coral_params = coral_params[sort(coral_param_idx), :]
 coral_param_names = coral_params.fieldname
 
 # Get updated parameter positions
-lin_ext_idx, mbrate_idx, coldiam_idx, fecundity_idx, dhw_tol_mean_idx =
+lin_ext_idx, mbrate_idx, fecundity_idx, dhw_tol_mean_idx =
     extract_param_group_idx.([coral_params], target_param_names())
+
+lin_ext_idx = sc_fg_param_idxs("linear_extension", coral_params)
+mbrate_idx = sc_fg_param_idxs("mb_rate", coral_params)
 
 sample_bounds = collect(zip(
     coral_params.lower_bound,
     coral_params.upper_bound
 ))
 
-# Adjust bounds for linear extensions, fecundity and initial mean DHW tolerance
-adjust_bounds!(sample_bounds, lin_ext_idx, 0.25, 1.8)
+# Adjust bounds for fecundity and initial mean DHW tolerance
 adjust_bounds!(sample_bounds, fecundity_idx, 0.5, 3.0)
 adjust_bounds!(sample_bounds, dhw_tol_mean_idx, 0.8, 3.0)
+
+# Set bounds for linear extension and mb rate from ecorrap data
+set_bounds!(sample_bounds, lin_ext_idx, lin_ext_lb, lin_ext_ub)
+set_bounds!(sample_bounds, mbrate_idx, mb_rate_lb, mb_rate_ub)
 
 # Add parameters for location-specific scaling
 n_groups = 5
 n_size_classes = 7
 n_limited_locs = length(LIMITED_LOCATIONS)
 n_factors = 3  # growth, mortality, fecundity
-
-# Adjust bounds for mortality rate
-# Size specific changes for each group
-for grp in 1:n_groups, sz in 1:n_size_classes
-    group_idx = extract_param_group_idx(coral_params, "$(grp)_$(sz)_mb_rate")
-
-    if sz < 4
-        sample_bounds[group_idx] .= fill((0.01, 0.6), length(group_idx))
-    else
-        ub = grp > 3 ? 0.15 : 0.4
-        sample_bounds[group_idx] .= fill((0.01, ub), length(group_idx))
-    end
-end
 
 location_coef = fill((0.3, 1.5), n_groups * n_limited_locs * n_factors)
 
@@ -175,8 +252,7 @@ function setup_run(
 
     insert_init_loc_cover!(
         new_dom,
-        sampled_params[param_idxs[7]:param_idxs[8]],
-        target_dom_idxs=loc_idxs
+        sampled_params[param_idxs[7]:param_idxs[8]], target_dom_idxs=loc_idxs
     )
 
     return new_dom, scen, growth_acc_params, scale_factors
