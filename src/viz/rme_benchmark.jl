7708dd40842f803e92238e9244885590a2b219bd
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-12-06 17:31:30 +1100

    WIP update diff_score_barplot

--- src/viz/rme_benchmark.jl
+++ src/viz/rme_benchmark.jl
@@ -11,13 +11,14 @@ function diff_score_barplot(
     legend_labels::Vector{String}=["", ""],
 )
     score_diffs = score_a .- score_b
+    n_locations::Int64 = length(n_observations)
     ylim_lower = floor(minimum([score_diffs..., n_observations...])) - 1
     ylim_upper = ceil(maximum([score_diffs..., n_observations...])) + 1
     limits = (nothing, (ylim_lower, ylim_upper))
 
     fig = Figure(size=fig_size)
 
-    n_locations::Int64 = length(n_observations)
+
     n_locs_1::Int64 = Int(floor(n_locations / 2))
     n_locs_2::Int64 = n_locations - n_locs_1
 
@@ -26,21 +27,31 @@ function diff_score_barplot(
     benchmark_scores_sorted = score_b[n_obs_sortperm]
     n_observations_sorted = n_observations[n_obs_sortperm]
 
-    # Split data into into two horizontal plots
-    ax_1 = Axis(fig[1, 1], title=title, titlesize=20, limits=limits)
-    ax_2 = Axis(fig[2, 1], xlabel=xlabel, xlabelsize=18, limits=limits)
-    dodge_diff_plot!(
-        ax_1, n_locs_1, rme_scores_sorted[1:n_locs_1], benchmark_scores_sorted[1:n_locs_1],
-        n_observations_sorted[1:n_locs_1], colors; dodge_gap=0.05, gap=0.3, direction=:y
-    )
-    dodge_diff_plot!(
-        ax_2, n_locs_2, rme_scores_sorted[n_locs_1+1:end],
-        benchmark_scores_sorted[n_locs_1+1:end], n_observations_sorted[n_locs_1+1:end],
-        colors; dodge_gap=0.05, gap=0.3, direction=:y
-    )
+    if n_locations > 100
+        # Split data into into two horizontal plots
+        ax_1 = Axis(fig[1, 1], title=title, titlesize=20, limits=limits)
+        ax_2 = Axis(fig[2, 1], xlabel=xlabel, xlabelsize=18, limits=limits)
+        dodge_diff_plot!(
+            ax_1, n_locs_1, rme_scores_sorted[1:n_locs_1], benchmark_scores_sorted[1:n_locs_1],
+            n_observations_sorted[1:n_locs_1], colors; dodge_gap=0.05, gap=0.3, direction=:y
+        )
+        dodge_diff_plot!(
+            ax_2, n_locs_2, rme_scores_sorted[n_locs_1+1:end],
+            benchmark_scores_sorted[n_locs_1+1:end], n_observations_sorted[n_locs_1+1:end],
+            colors; dodge_gap=0.05, gap=0.3, direction=:y
+        )
+        elements = [PolyElement(polycolor=colors[i]) for i in 1:length(legend_labels)]
+        Legend(fig[3, 1], elements, legend_labels, orientation=:horizontal)
+    else
+        ax = Axis(fig[1, 1], xlabel=xlabel, xlabelsize=18, limits=limits)
+        dodge_diff_plot!(
+            ax, n_locations, rme_scores_sorted, benchmark_scores_sorted,
+            n_observations_sorted, colors; dodge_gap=0.05, gap=0.3, direction=:y
+        )
+        elements = [PolyElement(polycolor=colors[i]) for i in 1:length(legend_labels)]
+        Legend(fig[2, 1], elements, legend_labels, orientation=:horizontal)
+    end
 
-    elements = [PolyElement(polycolor=colors[i]) for i in 1:length(legend_labels)]
-    Legend(fig[3, 1], elements, legend_labels, orientation=:horizontal)
 
     return fig
 end
