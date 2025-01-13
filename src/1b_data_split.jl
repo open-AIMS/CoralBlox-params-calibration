ef83d8cedc6b0c67cdefb0de8040aa6cd31db244
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2025-01-13 16:10:52 +1100

    Finish data split script

--- src/1b_data_split.jl
+++ src/1b_data_split.jl
@@ -3,6 +3,8 @@ Randomly split the available data into calibration and validation sets, ensuring
 both calibration and validation data for every bioregion group.
 """
 
+using Random
+
 # Geopackage containing both the bioregions and the grouped bioregion indices
 bioregion_groups_gpkg = GDF.read(BIOREGION_GROUPS_PATH)
 # Coral Composition Data
@@ -19,7 +21,7 @@ ltmp_reef_perm = sortperm(ltmp_reef_years) .+ 4
 ltmp_reef_data_names = names(ltmp_reef_data)
 ltmp_reef_data_names[5:end] .= ltmp_reef_data_names[ltmp_reef_perm]
 
-# Rorder columns
+# Re-order columns
 ltmp_reef_data = select!(ltmp_reef_data, ltmp_reef_data_names...)
 
 # Rescale to be proportions
@@ -34,14 +36,14 @@ first_yr_idx = findfirst(x -> x == "2008", names(ltmp_reef_data))
 """
 Get all non-unique elements of a list. Found on stack overflow https://stackoverflow.com/questions/54652787/julia-function-to-return-non-unique-elements-of-an-array
 """
-function nonunique(x::AbstractArray{T}) where T
+function nonunique(x::AbstractArray{T}) where {T}
     xs = sort(x)
     duplicatedvector = T[]
-    for i=2:length(xs)
-        if (isequal(xs[i],xs[i-1]) &&
+    for i = 2:length(xs)
+        if (isequal(xs[i], xs[i-1]) &&
             (length(duplicatedvector) == 0 || !isequal(duplicatedvector[end], xs[i])))
 
-            push!(duplicatedvector,xs[i])
+            push!(duplicatedvector, xs[i])
         end
     end
 
@@ -71,9 +73,10 @@ for non_uniq_id in non_unique_ltmp_locs
     delete!(ltmp_reef_data, to_delete)
 end
 
-# Since data has been aggregated between a few locaotions Reef IDs, GRMBPA IDs and
+# Since data has been aggregated between a few locations Reef IDs, GBRMPA IDs and
 # geometries are no longer accurate, remove these columns to prevent future errors
 select!(ltmp_reef_data, Not(:REEF_ID, :GBRMPA_ID))
+first_yr_idx = findfirst(x -> x == "2008", names(ltmp_reef_data))
 
 """
 To minimize the number of index vectors being passed around and copies being created, define
@@ -84,7 +87,7 @@ struct LocationDataStore
     # Data fields
     domain_gpkg::DataFrame
     ltmp_coral_cover::DataFrame
-    coral_composition::YAXArrays
+    coral_composition::YAXArray
     # Index Fields
     domain_to_ltmp_cover::Vector{Int64}
     domain_to_composition::Vector{Int64}
@@ -124,25 +127,25 @@ function domain_idx_to_ltmp_cover(loc_data_store::LocationDataStore, domain_idx:
 end
 
 """
-    domain_idx_to_compisition(loc_data_store::LocationDataStore, domain_idx::Int64)::Int64
+    domain_idx_to_composition(loc_data_store::LocationDataStore, domain_idx::Int64)::Int64
 
 Given the index of a location in the domain geopackage, return the index of the same
 location in the coral composition yaxarray. The majority of domain locations do not have
 a coral composition data.
 """
-function domain_idx_to_compisition(loc_data_store::LocationDataStore, domain_idx::Int64)::Int64
+function domain_idx_to_composition(loc_data_store::LocationDataStore, domain_idx::Int64)::Int64
     return loc_data_store.domain_to_composition[domain_idx]
 end
 
 """
-    enough_data(df_row::DataFrameRow)
+    sufficient_data(df_row::DataFrameRow)
 
 Ltmp Coral Cover locations with atleast 4 observations that span more then 10 years are
 considered to have sufficient data to be used for calibration.
 """
-function enough_data(df_row::DataFrameRow)
+function sufficient_data(df_row::DataFrameRow)
     raw_row = collect(df_row)
-    n_obs = count((!).(ismissing(df)))
+    n_obs = count(.!ismissing.(raw_row))
     if n_obs < 4
         return false
     end
@@ -154,3 +157,48 @@ function enough_data(df_row::DataFrameRow)
 
     return true
 end
+
+"""
+    split_indices(location_idxs::Vector{Int64}; train_proportion::Float64=0.75)
+
+# Return
+- Validation Split
+- Calibration Split
+"""
+function split_indices(location_idxs::Vector{Int64}; calibration_proportion::Float64=0.75)
+    n_locations = length(location_idxs)
+
+    n_validation_data::Int64 = Int(floor((1 - calibration_proportion) * n_locations))
+    min_validation_data = 2
+    if n_validation_data < min_validation_data
+        n_validation_data = min_validation_data
+    end
+    shuffled_idxs = shuffle(location_idxs)
+
+    return shuffled_idxs[1:n_validation_data], shuffled_idxs[n_validation_data+1:end]
+end
+
+unique_biogroup_ids = unique(bioregion_groups_gpkg.ASSIGNED_BIOREGION)
+sufficient_data_mask = sufficient_data.(eachrow(ltmp_reef_data[:, first_yr_idx:end]))
+ltmp_reef_data = ltmp_reef_data[sufficient_data_mask, :]
+
+ltmp_cover_to_domain = [
+    findfirst(
+        x -> x == uniq_id, dom.loc_data.UNIQUE_ID
+    ) for uniq_id in ltmp_reef_data.RME_UNIQUE_ID
+]
+ltmp_reef_data[!, :BIOGROUP_IDS] .= bioregion_groups_gpkg.ASSIGNED_BIOREGION[ltmp_cover_to_domain]
+
+biogroup_ltmp_idxs = [
+    findall(
+        ltmp_reef_data.BIOGROUP_IDS .== biogrp_id
+    ) for biogrp_id in unique_biogroup_ids
+]
+
+biogroup_data_splits = split_indices.(biogroup_ltmp_idxs; calibration_proportion=0.75)
+
+validation_splits = vcat(first.(biogroup_data_splits)...)
+calibration_splits = vcat(last.(biogroup_data_splits)...)
+
+validation_df = ltmp_reef_data[validation_splits, :]
+calibration_df = ltmp_reef_data[calibration_splits, :]
