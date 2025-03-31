a8e9f713289962888341417aac8fa192b7d0d183
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2025-04-01 09:26:11 +1100

    Delete create_error_statistics

--- src/common/perf_metrics.jl
+++ src/common/perf_metrics.jl
@@ -115,69 +115,6 @@ function constant_error_statistics(
     return err_csv
 end
 
-function create_error_statistics(filename::String)::DataFrame
-    @info "Computing Error Statistics for time period: $(START_YEAR) - $(END_YEAR)"
-
-    north_ind::Int64 = findfirst(x -> x >= START_YEAR, ltmp_north.Year)
-    central_ind::Int64 = findfirst(x -> x >= START_YEAR, ltmp_central.Year)
-    south_ind::Int64 = findfirst(x -> x >= START_YEAR, ltmp_south.Year)
-
-    north_end::Int64 = findfirst(x -> x >= END_YEAR, ltmp_north.Year) - 1
-    central_end::Int64 = findfirst(x -> x >= END_YEAR, ltmp_central.Year) - 1
-    south_end::Int64 = findfirst(x -> x >= END_YEAR, ltmp_south.Year) - 1
-
-    north_xs = ltmp_north.Year[north_ind:north_end]
-    central_xs = ltmp_central.Year[central_ind:central_end]
-    south_xs = ltmp_south.Year[south_ind:south_end]
-
-    north_resp = ltmp_north.response[north_ind:north_end]
-    central_resp = ltmp_central.response[central_ind:central_end]
-    south_resp = ltmp_south.response[south_ind:south_end]
-
-    s_rac_north = dropdims(mean(north_res[timesteps=At(north_xs)], dims=(:sites, :scenarios)), dims=(:sites, :scenarios))
-    s_rac_central = dropdims(mean(central_res[timesteps=At(central_xs)], dims=(:sites, :scenarios)), dims=(:sites, :scenarios))
-    s_rac_south = dropdims(mean(south_res[timesteps=At(south_xs)], dims=(:sites, :scenarios)), dims=(:sites, :scenarios))
-
-    rmse_north::Float64 = rmse(s_rac_north, north_resp)
-    rmse_central::Float64 = rmse(s_rac_central, central_resp)
-    rmse_south::Float64 = rmse(s_rac_south, south_resp)
-
-    @info "RMSE North: $(rmse_north), Central: $(rmse_central), South: $(rmse_south)"
-
-    # Coefficient of Determination
-    cc_north::Float64 = cor(s_rac_north, north_resp)
-    cc_central::Float64 = cor(s_rac_central, central_resp)
-    cc_south::Float64 = cor(s_rac_south, south_resp)
-
-    @info "Correlation Coefficient North: $(cc_north), Central: $(cc_central), South: $(cc_south)"
-
-    # MAEE
-    maee_north::Float64 = MAEE(s_rac_north, north_resp)
-    maee_central::Float64 = MAEE(s_rac_central, central_resp)
-    maee_south::Float64 = MAEE(s_rac_south, south_resp)
-
-    @info "Mean Absolute Exponential Error North: $(maee_north), Central: $(maee_central), South: $(maee_south)"
-
-    # bias
-    bias_north::Float64 = bias(s_rac_north, north_resp)
-    bias_central::Float64 = bias(s_rac_central, central_resp)
-    bias_south::Float64 = bias(s_rac_south, south_resp)
-
-    @info "Bias North: $(bias_north), Central: $(bias_central), South: $(bias_south)"
-
-    err_csv = DataFrame(
-        Regions=["North", "Central", "South"],
-        RMSE=[rmse_north, rmse_central, rmse_south],
-        R=[cc_north, cc_central, cc_south],
-        MAEE=[maee_north, maee_central, maee_south],
-        BIAS=[bias_north, bias_central, bias_south]
-    )
-
-    CSV.write(filename, err_csv, writeheader=true)
-
-    return err_csv
-end
-
 function collect_error_stats(
     raw_data,
     ltmp_loc_idx;
