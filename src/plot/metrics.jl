3007edadbc1622da9dfd966ed9c9f8d1894f969c
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2025-05-07 10:43:12 +1000

    Properly use GeoMakie to plot maps

--- src/plot/metrics.jl
+++ src/plot/metrics.jl
@@ -1,4 +1,5 @@
 using CairoMakie
+using GeoMakie
 
 function plot_rmse_scatter(rmse_diff, observation_type::String)
     _rmse_diff = sort(rmse_diff)
@@ -35,10 +36,18 @@ function plot_pcc_scatter(pcc)
 
     # Create scatter plot
     fig = Figure()
-    ax = Axis(fig[1, 1], title="Pearson Correlation Coefficient (PCC) Scatter Plot",
-        xlabel="Index", ylabel="PCC")
 
-    scatter!(ax, 1:length(pcc), pcc, color=:blue, label="PCC")
+    _yticks = sort(unique(vcat((-1:0.5:1), round(mean, digits=2))))
+    ax = Axis(
+        fig[1, 1],
+        title="Pearson Correlation Coefficient (PCC) Scatter Plot",
+        xlabel="Index",
+        ylabel="PCC",
+        yticks=(_yticks)
+    )
+
+
+    scatter!(ax, 1:length(pcc), sort(pcc), color=:blue, label="PCC")
 
     # Add horizontal line for mean
     hlines!(ax, [mean], color=:red, linestyle=:dash, label="Mean")
@@ -48,7 +57,17 @@ function plot_pcc_scatter(pcc)
     #     fill(lower_bound, length(pcc)), fill(upper_bound, length(pcc)),
     #     color=(0, 0, 1, 0.2), label="Bounds")
 
-    axislegend(ax)
+    legend_els = [
+        MarkerElement(color=:blue, marker=:circle),
+        LineElement(color=:red, linestyle=:dash),
+    ]
+
+    Legend(
+        fig[1, 2],
+        legend_els,
+        ["PCC", "Mean"]
+    )
+
     fig
 end
 
@@ -65,9 +84,18 @@ function plot_rmse_diff_map(
     rmse_diffs = benchmark_ .- rmse_
 
     fig_size = get(fig_opts, :size, FIG_SIZE[:map])
+    fig_title = get(fig_opts, :title, "Benchmark RMSE - Model RMSE")
     fig = Figure(size=fig_size)
-    ax = Axis(fig[1, 1], title="Benchmark RMSE - Model RMSE", titlesize=18, xlabelsize=15,
-        ylabelsize=15, xlabel="Longitude", ylabel="Latitude")
+    ax = GeoAxis(
+        fig[1, 1],
+        dest="+proj=latlong +datum=WGS84",
+        title=fig_title,
+        titlesize=18,
+        xlabelsize=15,
+        ylabelsize=15,
+        xlabel="Longitude",
+        ylabel="Latitude"
+    )
     domain_gpkg = observations.domain_gpkg
     observation_gpkg = domain_gpkg[domain_gpkg.UNIQUE_ID.∈[observations.ltmp_unique_ids], :]
 
@@ -88,7 +116,7 @@ function plot_rmse_diff_map(
     return fig
 end
 
-function plot_pearson_coeff_map(
+function plot_pcc_map(
     raw_data::Array{Float64,3};
     fig_opts::Dict=Dict(),
     observations::LocationDataStore=COMBINED_STORE,
@@ -101,20 +129,29 @@ function plot_pearson_coeff_map(
     rmse_, benchmark_, cc_, maee_, bias_ = eachrow(hcat(map(collect, error_stats)...))
 
     fig_size = get(fig_opts, :size, FIG_SIZE[:map])
+    fig_title = get(fig_opts, :title, "Pearson Correlation Coefficient")
     fig = Figure(size=fig_size)
-    ax = Axis(fig[1, 1], title="Pearson's Correlation Coefficient", titlesize=18, xlabelsize=15,
-        ylabelsize=15, xlabel="Longitude", ylabel="Latitude")
+    ax = GeoAxis(
+        fig[1, 1],
+        dest="+proj=latlong +datum=WGS84",
+        xlabel="Longitude",
+        ylabel="Latitude",
+        title=fig_title,
+        titlesize=18,
+        # xlabelsize=15,
+        # ylabelsize=15,
+    )
     domain_gpkg = observations.domain_gpkg
     observation_gpkg = domain_gpkg[domain_gpkg.UNIQUE_ID.∈[observations.ltmp_unique_ids], :]
     poly!.(ax, domain_gpkg.geom, color=:gray)
-    poly!.(ax, observation_gpkg.geom, color=:red)
+
     scatter!(ax, observation_gpkg.X_COORD, observation_gpkg.Y_COORD, markersize=35,
         color=cc_, colormap=:bam, alpha=0.8, strokewidth=1, strokecolor=(:gray, 0.1))
     max_val, min_val = extrema(cc_)
     up_limit = maximum(abs.((max_val, min_val)))
     lower_limit = -up_limit
     Colorbar(fig[1, 2], limits=(lower_limit, up_limit), colormap=:bam,
-        label="Model RMSE - Benchmark RMSE")
+        label="Pearson Correlation Coefficient")
 
     return fig
 end
