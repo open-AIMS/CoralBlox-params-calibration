1c77028682cece55d88e8501608bbe157117f770
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2025-09-18 11:37:32 +1000

    Add :short title in _location_err_title fig_opts

--- src/plot/plot_helpers.jl
+++ src/plot/plot_helpers.jl
@@ -1,12 +1,24 @@
 """
-    _location_err_title(raw_data, ltmp_loc_idx; domain_idx=ALL_LTMP_IDXS[ltmp_loc_idx], reef_name=dom.loc_data.GBR_NAME[domain_idx], reef_id=ltmp_reef_data.RME_UNIQUE_ID[ltmp_loc_idx])::Makie.RichText
+    _location_err_title(
+        raw_data,
+        ltmp_loc_idx;
+        fig_opts::Dict{Symbol,Any}=Dict{Symbol,Any}(),
+        observations::LocationDataStore=COMBINED_STORE,
+    )::Makie.RichText
 
 Construct a Rich Test string containing the location unique id, location name and the error
 statistics for the location.
+
+
+# Arguments
+- `fig_opts` :
+    - `:short` : If true, display only RMSE and SCC. Otherwise shows RMSE, μ bnch, PCC, SCC,
+    MAEE and BIAS
 """
 function _location_err_title(
     raw_data,
     ltmp_loc_idx;
+    fig_opts::Dict{Symbol,Any}=Dict{Symbol,Any}(),
     observations::LocationDataStore=COMBINED_STORE,
 )::Makie.RichText
     domain_idx::Int64 = ltmp_cover_idx_to_domain(observations, ltmp_loc_idx)
@@ -20,12 +32,16 @@ function _location_err_title(
     rmse_, benchmark_, cc_, maee_, bias_, scc_ = collect_error_stats(
         raw_data, ltmp_loc_idx; observations=observations
     )
-    rmse_, benchmark_, cc_, maee_, bias_ = trunc.(
-        [rmse_, benchmark_, cc_, maee_, bias_], digits=4
+    rmse_, benchmark_, cc_, maee_, bias_, scc_ = trunc.(
+        [rmse_, benchmark_, cc_, maee_, bias_, scc_], digits=4
     )
 
-    err_report_str = "RMSE: $(rmse_) | μ bnch: $(benchmark_) | "
-    err_report_str *= "PCC: $(cc_) | MAEE: $(maee_) | BIAS: $(bias_)"
+    err_report_str = if get(fig_opts, :short, true)
+        "RMSE: $(rmse_) | SCC: $(scc_)"
+    else
+        "RMSE: $(rmse_) | μ bnch: $(benchmark_) | " *
+        "PCC: $(cc_) | SCC: $(scc_) | MAEE: $(maee_) | BIAS: $(bias_)"
+    end
 
     title_text = rich("$(reef_name)\n$(reef_id)\n", rich(err_report_str, fontsize=15))
 
