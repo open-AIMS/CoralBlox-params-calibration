e28538713b8ca94584439d5e49909c7f3a19ed10
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2025-09-18 11:27:16 +1000

    Generalize pcc_locs to allow for other correlation metrics

--- src/3c_metric_analysis.jl
+++ src/3c_metric_analysis.jl
@@ -2,6 +2,7 @@
 metrics_save_dir::String = joinpath(OUT_DIR, "metrics")
 mkpath(metrics_save_dir)
 
+# Maps
 rmse_diff_map = plot_rmse_diff_map(
     rs_raw.raw;
     observations=VALIDATION_STORE,
@@ -9,13 +10,23 @@ rmse_diff_map = plot_rmse_diff_map(
 )
 save(joinpath(metrics_save_dir, "rmse_diff_map.png"), rmse_diff_map)
 
-pearson_coeff_map = plot_pcc_map(
+pearson_coeff_map = plot_metric_map(
     rs_raw.raw;
     observations=VALIDATION_STORE,
+    metric_type=:pcc,
     fig_opts=Dict(:title => "Pearson Correlation Coefficient\nValidation Locations")
 )
 save(joinpath(metrics_save_dir, "pcc_map.png"), pearson_coeff_map)
 
+scc_map = plot_metric_map(
+    rs_raw.raw;
+    observations=VALIDATION_STORE,
+    metric_type=:scc,
+    fig_opts=Dict(:title => "Spearman Correlation Coefficient\nValidation Locations")
+)
+save(joinpath(metrics_save_dir, "scc_map.png"), scc_map)
+
+# Scatter
 rmse_diff_validation = sort(rmse_diff(rs_raw.raw, VALIDATION_STORE))
 f_rmse_diff_validation = plot_rmse_scatter(rmse_diff_validation; observation_type="Validation")
 save(joinpath(metrics_save_dir, "rmse_diff_validation.png"), f_rmse_diff_validation)
@@ -24,14 +35,51 @@ rmse_diff_calibration = sort(rmse_diff(rs_raw.raw, CALIBRATION_STORE))
 f_rmse_diff_calibration = plot_rmse_scatter(rmse_diff_calibration; observation_type="Calibration")
 save(joinpath(metrics_save_dir, "rmse_diff_calibration.png"), f_rmse_diff_calibration)
 
-pcc_validation = pcc_locs(rs_raw.raw, VALIDATION_STORE)
-f_pcc_validation = plot_pcc_scatter(pcc_validation; observation_type="Validation")
+scc_validation = location_correlation_coefficients(rs_raw.raw, VALIDATION_STORE)
+f_scc_validation = plot_metric_scatter(
+    scc_validation;
+    observation_type="Validation",
+    axis_opts=Dict{Symbol,Any}(
+        :title => "Spearman's Correlation Coefficient (SCC)\nValidation reefs",
+        :ylabel => "SCC"
+    ),
+    opts=Dict{Symbol,Any}(:metric_label => "SCC")
+)
+save(joinpath(metrics_save_dir, "scc_validation.png"), f_scc_validation)
+
+scc_calibration = location_correlation_coefficients(rs_raw.raw, CALIBRATION_STORE)
+f_scc_calibration = plot_metric_scatter(
+    scc_calibration;
+    observation_type="Calibration",
+    axis_opts=Dict{Symbol,Any}(
+        :title => "Spearman's Correlation Coefficient (SCC)\nCalibration reefs",
+        :ylabel => "SCC"
+    ),
+    opts=Dict{Symbol,Any}(:metric_label => "SCC")
+)
+save(joinpath(metrics_save_dir, "scc_calibration.png"), f_scc_calibration)
+
+pcc_validation = location_correlation_coefficients(rs_raw.raw, VALIDATION_STORE; correlation_metric=:pearson)
+f_pcc_validation = plot_metric_scatter(pcc_validation;
+    observation_type="Validation",
+    axis_opts=Dict{Symbol,Any}(
+        :title => "Pearson's Correlation Coefficient (SCC)\nValidation reefs",
+        :ylabel => "PCC"
+    ),
+    opts=Dict{Symbol,Any}(:metric_label => "PCC")
+)
 save(joinpath(metrics_save_dir, "pcc_validation.png"), f_pcc_validation)
 
-pcc_calibration = pcc_locs(rs_raw.raw, CALIBRATION_STORE)
-f_pcc_calibration = plot_pcc_scatter(pcc_calibration; observation_type="Calibration")
+pcc_calibration = location_correlation_coefficients(rs_raw.raw, CALIBRATION_STORE; correlation_metric=:pearson)
+f_pcc_calibration = plot_metric_scatter(pcc_calibration;
+    observation_type="Calibration",
+    axis_opts=Dict{Symbol,Any}(
+        :title => "Pearson's Correlation Coefficient (SCC)\nCalibration reefs",
+        :ylabel => "PCC"
+    ),
+    opts=Dict{Symbol,Any}(:metric_label => "PCC")
+)
 save(joinpath(metrics_save_dir, "pcc_calibration.png"), f_pcc_calibration)
 
-include("common/common.jl")
 fig_m_heatmap = plot_metrics_heatmap(rs_raw.raw; fig_size=(700, 700), observations=VALIDATION_STORE)
 save(joinpath(metrics_save_dir, "metrics_heatmap.png"), fig_m_heatmap)
