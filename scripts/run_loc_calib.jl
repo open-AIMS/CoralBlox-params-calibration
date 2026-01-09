d2c660c37ed0c828772d433635a883f1a8366621
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2026-01-09 16:27:55 +1000

    Update scripts names in run_loc_calib.jl

--- scripts/run_loc_calib.jl
+++ scripts/run_loc_calib.jl
@@ -1,6 +1,6 @@
 cd("src")
 @info pwd()
 
-include("../src/1_setup.jl")
-include("../src/1b_data_split.jl")
-include("../src/2_location_calibration.jl")
+include("../src/01_a_setup.jl")
+include("../src/01_b_data_split.jl")
+include("../src/02_location_calibration.jl")
