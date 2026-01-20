e7d76f540dd99ef9064ab08bac940fc66b59a2df
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2026-01-20 16:23:54 +1100

    Use median instead of mean to aggregate SRCC

--- src/analysis/regional_analysis.jl
+++ src/analysis/regional_analysis.jl
@@ -36,19 +36,24 @@ function _region_stats(
         n_locations::Int64
     }
 }
-    target_can_reefs_mask = dom.loc_data[region_mask, :].UNIQUE_ID .∈ Ref(observations.ltmp_unique_ids)
+    @views target_reefs_mask =
+        dom.loc_data[region_mask, :].UNIQUE_ID .∈ Ref(observations.ltmp_unique_ids)
 
-    if iszero(target_can_reefs_mask)
+    if iszero(target_reefs_mask)
         return nothing
     end
 
-    target_can_reefs_ids = dom.loc_data[region_mask, :][target_can_reefs_mask, :UNIQUE_ID]
-    target_historic = observations.ltmp_coral_cover[observations.ltmp_unique_ids.∈Ref(target_can_reefs_ids), :]
+    target_can_reefs_ids = dom.loc_data[region_mask, :][target_reefs_mask, :UNIQUE_ID]
+    target_historic = observations.ltmp_coral_cover[
+        observations.ltmp_unique_ids .∈ Ref(target_can_reefs_ids), :
+    ]
 
     # Select the locations within target region and, from those, the locations within the
     # observation store
     # Also converts rs_raw data from relative to habitable area to relative to total area
-    target_model_data = @view(_loc_cover(rs_raw.raw)[:, region_mask][:, target_can_reefs_mask])
+    target_model_data = @view(
+        _loc_cover(rs_raw.raw)[:, region_mask][:, target_reefs_mask]
+    )
 
     n_locations, n_timesteps = size(target_historic)
     model_stats = Array{Float64}(fill(0.0, 3, n_timesteps))
@@ -59,7 +64,9 @@ function _region_stats(
         model_stats[:, y] .= quantile(@view(target_model_data[y, :]), [0.025 0.5 0.975])'
 
         # Fill historic median and confint
-        target_historic_data = @view(target_historic[.!ismissing.(target_historic[:, y]), y])
+        target_historic_data = @view(
+            target_historic[.!ismissing.(target_historic[:, y]), y]
+        )
         isempty(target_historic_data) && continue
         historic_stats[:, y] .= quantile(target_historic_data, [0.025 0.5 0.975])'
     end
@@ -67,10 +74,8 @@ function _region_stats(
     _rmse::Vector{Float64} = zeros(Float64, n_locations)
     _mae::Vector{Float64} = zeros(Float64, n_locations)
     _srcc::Vector{Float64} = zeros(Float64, n_locations)
-    _weights::Vector{Float64} = zeros(Float64, n_locations)
     for loc_idx in 1:n_locations
         has_data_mask = .!ismissing.(target_historic[loc_idx, :])
-        _weights[loc_idx] = sum(has_data_mask)
 
         mod_data = target_model_data[:, loc_idx]'[has_data_mask]
         obs_data = Vector{Float64}(target_historic[loc_idx, :][has_data_mask])
@@ -79,17 +84,12 @@ function _region_stats(
         _srcc[loc_idx] = corspearman(mod_data, obs_data)
     end
 
-    # Mean is weighted by the number of observations (years with data) in each location
-    _mean_rmse::Float64 = mean(_rmse, weights(_weights))
-    _mean_mae::Float64 = mean(_mae, weights(_weights))
-    _mean_srcc::Float64 = average_cc(_srcc; w=_weights)
-
     return (
         model_stats=model_stats,
         historic_stats=historic_stats,
-        rmse=_mean_rmse,
-        mae=_mean_mae,
-        srcc=_mean_srcc,
+        rmse=median(_rmse),
+        mae=median(_mae),
+        srcc=median(_srcc),
         n_locations=n_locations
     )
 end
